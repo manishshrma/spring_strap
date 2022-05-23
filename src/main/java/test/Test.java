23d982e0f95d9f68935ca7a57626774493297aaa
Sharma <2154508@cognizant.com>
2022-05-24 02:17:23 +0530

    add the second line

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,5 +6,6 @@ public class Test {
     {
         System.out.println("hello");
         System.out.println("made the first commit");
+        System.out.println("made the second commit");
     }
 }
